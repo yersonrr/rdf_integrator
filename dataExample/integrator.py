401b48630335c2babbc3c2fe7ab2102ba25e87a0
Yerson <yerson132@gmail.com>
2018-04-06 19:03:49 +0200

    Added new Visualization tool for RDF KG in Cytoscape.

--- dataExample/integrator.py
+++ dataExample/integrator.py
@@ -280,109 +280,7 @@ def newData():
 	return (node, edge)
 
 
-def integrator(config_file):
-	config = configparser.ConfigParser()
-	config.read(config_file)
-	
-	save_path = config.get('RDFData','pathToSave')
-	completeName = os.path.join(save_path, "new_rdfGraph.nt")         
-
-	F = open(completeName, "w")
-
-	file_name1 = config.get('RDFData','rdf1')
-	file_name2 = config.get('RDFData','rdf2')
-	file_ontologies = config.get('RDFData','ontology')
-
-	g=rdflib.Graph()
-	result1 = g.parse(location=file_name1, format="nt")
-	g2 = rdflib.Graph()
-	result2 = g2.parse(location=file_name2, format="nt")
-	subjects = []
-	predicates = []
-	objects = []
-	subjects2 = []
-	predicates2 = []
-	objects2 = []
-
-	for subject,predicate,obj in g:
-	    subjects.append(subject)
-	    predicates.append(predicate)
-	    if 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' == str(predicate):
-	    	class_identifier = predicate
-	    objects.append(obj)
-
-	for subject,predicate,obj in g2:
-	    subjects2.append(subject)
-	    predicates2.append(predicate)
-	    objects2.append(obj)
-
-	subjects = list(set(subjects))
-	subjects2 = list(set(subjects2))
-
-	number_classes = int(config.get('RDFData','number_classes'))
-
-	FileAux = open('auxFile.nt', 'w+')
-
-	f = open(file_ontologies, 'r')
-	for line in f.readlines():
-		FileAux.write(line)
-	f.close()
-
-	f = open(file_name1, 'r')
-	for line in f.readlines():
-		FileAux.write(line)
-	f.close()
-
-	f = open(file_name2, 'r')
-	for line in f.readlines():
-		FileAux.write(line)
-	f.close()
-	FileAux.close()
-
-	ontology_path = os.path.realpath(FileAux.name)
-
-	url = "http://localhost:9000/similarity/initialize?model_1="+ontology_path
-	headers = {'content-type': "application/json"}
-	response = requests.get(url, headers=headers)
-
-	url = "http://localhost:9001/setlocation"
-	data = {"location1":file_name1, 
-		"location2":file_name2}
-	data = json.dumps(data)
-	response = requests.post(url, data=data, headers=headers)
-
-	for n in range(number_classes):
-		integratePerClass(g, g2, subjects, subjects2, n, F, config, class_identifier)
-
-	getUnusedNodes(subjects, subjects2, g, g2, allJoins, F)
-
-	F.close()
-
-	for line in fileinput.input(completeName, inplace=True):
-		global mergedMolecules
-		replace = False
-		for elem in mergedMolecules:
-			if (line.find(elem['uri1']) > 0):
-				print(line.replace(elem['uri1'],elem['newUri'])[:-1])
-				replace = True
-			elif (line.find(elem['uri2']) > 0):
-				print(line.replace(elem['uri2'],elem['newUri'])[:-1])
-				replace = True
-		if not (replace):
-			print(line[:-1])
-
-	g = rdflib.Graph()
-	result = g.parse(location=completeName, format="nt")
-
-	file = open(completeName, "w")
-	for s,p,o in g:
-			elem = s.n3() + ' '
-			elem += p.n3() + ' '
-			obj = o.n3().replace('"', '\"')
-			elem += obj + '.\n'
-			file.write(elem)
-	
-	file.close()
+def visualization(allJoins, file_name1, file_name2):
 
 	g=rdflib.Graph()
 	result1 = g.parse(location=file_name1, format="nt")
@@ -560,6 +458,115 @@ def integrator(config_file):
 
 	with open('result.json', 'w') as fp:
 		json.dump(jsonData, fp)
+
+	return 0
+
+
+def integrator(config_file):
+	config = configparser.ConfigParser()
+	config.read(config_file)
+	
+	save_path = config.get('RDFData','pathToSave')
+	completeName = os.path.join(save_path, "new_rdfGraph.nt")         
+
+	F = open(completeName, "w")
+
+	file_name1 = config.get('RDFData','rdf1')
+	file_name2 = config.get('RDFData','rdf2')
+	file_ontologies = config.get('RDFData','ontology')
+
+	g=rdflib.Graph()
+	result1 = g.parse(location=file_name1, format="nt")
+	g2 = rdflib.Graph()
+	result2 = g2.parse(location=file_name2, format="nt")
+	subjects = []
+	predicates = []
+	objects = []
+	subjects2 = []
+	predicates2 = []
+	objects2 = []
+
+	for subject,predicate,obj in g:
+	    subjects.append(subject)
+	    predicates.append(predicate)
+	    if 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' == str(predicate):
+	    	class_identifier = predicate
+	    objects.append(obj)
+
+	for subject,predicate,obj in g2:
+	    subjects2.append(subject)
+	    predicates2.append(predicate)
+	    objects2.append(obj)
+
+	subjects = list(set(subjects))
+	subjects2 = list(set(subjects2))
+
+	number_classes = int(config.get('RDFData','number_classes'))
+
+	FileAux = open('auxFile.nt', 'w+')
+
+	f = open(file_ontologies, 'r')
+	for line in f.readlines():
+		FileAux.write(line)
+	f.close()
+
+	f = open(file_name1, 'r')
+	for line in f.readlines():
+		FileAux.write(line)
+	f.close()
+
+	f = open(file_name2, 'r')
+	for line in f.readlines():
+		FileAux.write(line)
+	f.close()
+	FileAux.close()
+
+	ontology_path = os.path.realpath(FileAux.name)
+
+	url = "http://localhost:9000/similarity/initialize?model_1="+ontology_path
+	headers = {'content-type': "application/json"}
+	response = requests.get(url, headers=headers)
+
+	url = "http://localhost:9001/setlocation"
+	data = {"location1":file_name1, 
+		"location2":file_name2}
+	data = json.dumps(data)
+	response = requests.post(url, data=data, headers=headers)
+
+	for n in range(number_classes):
+		integratePerClass(g, g2, subjects, subjects2, n, F, config, class_identifier)
+
+	getUnusedNodes(subjects, subjects2, g, g2, allJoins, F)
+
+	F.close()
+
+	for line in fileinput.input(completeName, inplace=True):
+		global mergedMolecules
+		replace = False
+		for elem in mergedMolecules:
+			if (line.find(elem['uri1']) > 0):
+				print(line.replace(elem['uri1'],elem['newUri'])[:-1])
+				replace = True
+			elif (line.find(elem['uri2']) > 0):
+				print(line.replace(elem['uri2'],elem['newUri'])[:-1])
+				replace = True
+		if not (replace):
+			print(line[:-1])
+
+	g = rdflib.Graph()
+	result = g.parse(location=completeName, format="nt")
+
+	file = open(completeName, "w")
+	for s,p,o in g:
+			elem = s.n3() + ' '
+			elem += p.n3() + ' '
+			obj = o.n3().replace('"', '\"')
+			elem += obj + '.\n'
+			file.write(elem)
+	
+	file.close()
+
+	visualization(allJoins, file_name1, file_name2)
 
 
 def readConfig():
